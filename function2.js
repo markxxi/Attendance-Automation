6d9247bb0dc7f3efb4d117e1e3099be5047363ee
markxxi <151999475+markxxi@users.noreply.github.com>
2025-03-10 07:03:41 +0000

    search input by division and name working

--- function2.js
+++ function2.js
@@ -482,10 +482,14 @@ function searchByName() {
     var tr = table.getElementsByTagName("tr");
 
     for (var i = 1; i < tr.length; i++) {
-        var td = tr[i].getElementsByTagName("td")[1]; 
-        if (td) {
-            var txtValue = td.textContent || td.innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        var td1 = tr[i].getElementsByTagName("td")[1];
+        var td2 = tr[i].getElementsByTagName("td")[2];
+        
+        if (td1 && td2) {
+            var txtValue1 = td1.textContent || td1.innerText;
+            var txtValue2 = td2.textContent || td2.innerText;
+            
+            if (txtValue1.toUpperCase().indexOf(filter) > -1 || txtValue2.toUpperCase().indexOf(filter) > -1) {
                 tr[i].style.display = "";
             } else {
                 tr[i].style.display = "none";
@@ -493,3 +497,4 @@ function searchByName() {
         }
     }
 }
+
